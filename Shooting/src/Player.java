5950530948d25b87a249810948e73bb7c684b6ad
髙木　瑛俊 <133090478+shun0917@users.noreply.github.com>
2024-06-18 10:34:53 +0900

    10.4プレイヤーをキーボードで動かす

--- Shooting/src/Player.java
+++ Shooting/src/Player.java
@@ -1,11 +1,52 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class Player extends Character{
-
+public class Player extends Character implements KeyListener {
 	public Player(double x, double y, double vx, double vy) {
 		super(x, y, vx, vy);
-		// TODO 自動生成されたコンストラクター・スタブ
+	}
+	
+	public void move() {
+		super.move();
+		if(x <0) {
+			x=0;
+		}
+		if(x>370) {
+			x =370;
+		}
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
 		
 	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+		if(e.getKeyCode() == KeyEvent.VK_LEFT) {
+			vx =-5;
+		}
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			vx =5;
+		}
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+		if(e.getKeyCode() == KeyEvent.VK_LEFT) {
+			vx =0;
+		}
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			vx =0;
+		}
+		
+	}
+	
+
 	
 
 }
