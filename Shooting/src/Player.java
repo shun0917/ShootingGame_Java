037e217ad79e293987b1b1c3c4e931dfe0cf7e89
髙木　瑛俊 <133090478+shun0917@users.noreply.github.com>
2024-06-18 11:16:51 +0900

    10.8プレイヤーの絵柄を変える

--- Shooting/src/Player.java
+++ Shooting/src/Player.java
@@ -6,6 +6,14 @@ public class Player extends Character implements KeyListener {
 		super(x, y, vx, vy);
 	}
 	
+	public void draw (MyFrame f){
+		f.setColor(0,128,0);
+		f.fillRect(x, y+20, 30, 10);
+		f.setColor(200, 200, 200);
+		f.fillRect(x+10, y, 10, 30);
+		
+	}
+	
 	public void move() {
 		super.move();
 		if(x <0) {
