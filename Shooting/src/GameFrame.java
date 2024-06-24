06c33b2a001da9f9db5ef22d016b469d81c2b52a
髙木　瑛俊 <133090478+shun0917@users.noreply.github.com>
2024-06-24 12:17:10 +0900

    12.2プレイヤー弾と敵の衝突判定

--- Shooting/src/GameFrame.java
+++ Shooting/src/GameFrame.java
@@ -6,9 +6,9 @@ public class GameFrame extends MyFrame {
 		GameWorld.playerBullets = new Vector<PlayerBullet>();
 
 		addKeyListener(GameWorld.player);
-		
-		GameWorld.enemies=new Vector<Enemy>();
-		GameWorld.enemies.add(new EnemyBase(100,50,1,0));
+
+		GameWorld.enemies = new Vector<Enemy>();
+		GameWorld.enemies.add(new EnemyBase(100, 50, 1, 0));
 
 		while (true) {
 			clear();
@@ -16,11 +16,13 @@ public class GameFrame extends MyFrame {
 			GameWorld.player.move();
 
 			movePlayerBullets();
-			
+
 			moveEnemies();
-			
+
 			checkPlayerAndEnemies();
-			
+
+			checkPlayerBulletsAndEnemies();
+
 			sleep(0.03);
 		}
 	}
@@ -39,15 +41,15 @@ public class GameFrame extends MyFrame {
 			}
 		}
 	}
-	
+
 	public void moveEnemies() {
-		for (int i=0; i<GameWorld.enemies.size(); i++) {
-			Enemy e=GameWorld.enemies.get(i);
+		for (int i = 0; i < GameWorld.enemies.size(); i++) {
+			Enemy e = GameWorld.enemies.get(i);
 			e.draw(this);
 			e.move();
 		}
 	}
-	
+
 	public void checkPlayerAndEnemies() {
 		for (int i = 0; i < GameWorld.enemies.size(); i++) {
 
@@ -60,6 +62,32 @@ public class GameFrame extends MyFrame {
 			}
 		}
 	}
-	
 
+	public void checkPlayerBulletsAndEnemies() {
+		int i = 0;
+		while (i < GameWorld.playerBullets.size()) {
+			//プレイヤー弾１つ１つについて、変数bに入れて繰り返し実行する
+			PlayerBullet b = GameWorld.playerBullets.get(i);
+			int j = 0;
+			int hits = 0;
+			while (j < GameWorld.enemies.size()) {
+				//敵１つ１つについて、変数eに入れて繰り返し実行する
+				Enemy e = GameWorld.enemies.get(j);
+				//敵eとプレイヤーの弾bが衝突していたら「あたり」と表示する
+				if (Math.abs(e.x - b.x) <= 30 &&
+						Math.abs(e.y - b.y) <= 30) {
+					System.out.println("あたり");
+					hits++;
+					GameWorld.enemies.remove(j);
+				} else {
+					j++;
+				}
+			}
+			if (hits > 0) {
+				GameWorld.playerBullets.remove(i);
+			} else {
+				i++;
+			}
+		}
+	}
 }
