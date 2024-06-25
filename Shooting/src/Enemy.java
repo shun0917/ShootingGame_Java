3f569137e6af7687c90b7fe295b1db1ff81e1b21
髙木　瑛俊 <133090478+shun0917@users.noreply.github.com>
2024-06-25 10:02:25 +0900

    12.5敵の種類によって耐久力を変更する

--- Shooting/src/Enemy.java
+++ Shooting/src/Enemy.java
@@ -1,7 +1,7 @@
 
 public class Enemy extends Character{
 	
-	int life=3;
+	int life=1;
 	public Enemy (double x, double y, double vx, double vy) {
 		super(x, y, vx, vy);
 	}
--- Shooting/src/EnemyBase.java
+++ Shooting/src/EnemyBase.java
@@ -2,6 +2,7 @@
 public class EnemyBase extends Enemy{
 	public EnemyBase (double x, double y, double vx, double vy) {
 		super(x, y, vx, vy);
+		life=20;
 	}
 	public void draw(MyFrame f) {
 		f.setColor(0, 128,0);
